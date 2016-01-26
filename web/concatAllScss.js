ba9a97e7f06ac467e8fdedadfba29b46e80412f8
Virginia.wu <Virginia.wu@trantect.com>
2016-01-26 22:21:05 +0800

    modify @import pwd in main.scss use script

--- web/concatAllScss.js
+++ web/concatAllScss.js
@@ -5,7 +5,7 @@ function writeName(_list){
     return '@import "' + file + '"'
   })
   var _str = _nameList.join(';\n');
-  fs.writeFile('assets/styles/scss/tmp/main.scss', _str, function (err) {
+  fs.writeFile('assets/styles/scss/main.scss', _str, function (err) {
   	if (err){
   		console.log(err);
   	}
@@ -14,20 +14,20 @@ function writeName(_list){
 
 (function main() {
   var totalList = fs.readdirSync("assets/styles/scss/variables").map(function (file) {
-    return "../variables/" + file
+    return "./variables/" + file
   })
   .concat(fs.readdirSync("assets/styles/scss/mixins").map(function (file) {
-    return "../mixins/" + file
+    return "./mixins/" + file
   }))
-  .concat(['../_base.scss'])
+  .concat(['./_base.scss'])
   .concat(fs.readdirSync("assets/styles/scss/states").map(function (file) {
-    return "../states/" + file
+    return "./states/" + file
   }))
   .concat(fs.readdirSync("assets/styles/scss/modules").map(function (file) {
-    return "../modules/" + file
+    return "./modules/" + file
   }))
   .concat(fs.readdirSync("assets/styles/scss/layout").map(function (file) {
-    return "../layout/" + file
+    return "./layout/" + file
   }))
   writeName(totalList);
 }).call(this);
